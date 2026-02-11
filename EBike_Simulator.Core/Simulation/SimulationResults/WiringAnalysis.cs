ad1e9da5df4415478c68f8a54d1bd5d1c6b9152d
Infsec <Infsec@HOME-PC>
2026-02-11 21:38:12 +0300

    string to stringbuilder change

--- EBike_Simulator.Core/Simulation/SimulationResults/WiringAnalysis.cs
+++ EBike_Simulator.Core/Simulation/SimulationResults/WiringAnalysis.cs
@@ -1,4 +1,5 @@
 ﻿using EBike_Simulator.Core.BikeComponents;
+using System.Text;
 
 namespace EBike_Simulator.Core.Simulation.SimulationResults
 {
@@ -30,21 +31,24 @@ namespace EBike_Simulator.Core.Simulation.SimulationResults
         /// </summary>
         public string PrintReport()
         {
-            string output =
-    "\n=== АНАЛИЗ ПРОВОДКИ ===" +
-    $"\nПиковый ток (Батарея → Контроллер): {MaxBatteryCurrent:F1} А" +
-    $"\nПиковый ток (Контроллер → Мотор): {MaxMotorCurrent:F1} А" +
-   
-    string.Join("", RecommendedWires.Select(kvp =>
-        $"\n{kvp.Key}:" +
-        $"\n  Провод: ({kvp.Value.AWG} AWG)" +
-        $"\n  Сечение: {kvp.Value.CrossSectionMm2:F2} мм²" +
-        $"\n  Макс. ток: {kvp.Value.MaxCurrentAmp} А" +
-        $"\n  Безопасность: {kvp.Value.GetSafetyStatus(kvp.Key.Contains("Battery") ? MaxBatteryCurrent : MaxMotorCurrent)}"
-    )) +
-    $"\n\nСредние потери в проводке: {EstimatedAvgPowerLoss:F2} Вт";
+            var result = new StringBuilder();
 
-            return output ;
+            result.AppendLine("\n=== АНАЛИЗ ПРОВОДКИ ===");
+            result.AppendLine($"Пиковый ток (Батарея → Контроллер): {MaxBatteryCurrent:F1} А");
+            result.AppendLine($"Пиковый ток (Контроллер → Мотор): {MaxMotorCurrent:F1} А");
+
+            foreach (var kvp in RecommendedWires)
+            {
+                result.AppendLine($"\n{kvp.Key}:");
+                result.AppendLine($"  Провод: ({kvp.Value.AWG} AWG)");
+                result.AppendLine($"  Сечение: {kvp.Value.CrossSectionMm2:F2} мм²");
+                result.AppendLine($"  Макс. ток: {kvp.Value.MaxCurrentAmp} А");
+                result.AppendLine($"  Безопасность: {kvp.Value.GetSafetyStatus(kvp.Key.Contains("Battery") ? MaxBatteryCurrent : MaxMotorCurrent)}");
+            }
+
+            result.AppendLine($"\nСредние потери в проводке: {EstimatedAvgPowerLoss:F2} Вт");
+
+            return result.ToString();
         }
 
         #endregion
